02f57e791409e411d29cb44a6b38874fe66bcc78
Kevin <abc96007@gmail.com>
2021-12-16 18:30:13 +0800

    CORS

--- WebApplication1/Models/SalesOrderDetail.cs
+++ WebApplication1/Models/SalesOrderDetail.cs
@@ -10,8 +10,8 @@ namespace WebApplication1.Models
         public int SalesOrderDetailId { get; set; }
         public int SalesOrderId { get; set; }
         public int ProductId { get; set; }
-        public int Quantity { get; set; }
-        public decimal? Price { get; set; }
+        public int SalesQuantity { get; set; }
+        public decimal? SalesPrice { get; set; }
 
         public virtual Product Product { get; set; }
         public virtual SalesOrderHeader SalesOrder { get; set; }
